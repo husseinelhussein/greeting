96d2559e0a0bbfc87d3fc1cbac01122688711e33
Hussein <hussein@devvly.com>
2021-09-24 23:45:36 +0300

    added greeting functionality

--- app/Http/Controllers/Api/GreetingController.php
+++ app/Http/Controllers/Api/GreetingController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreGreetingRequest;
+use App\Http\Resources\GreetingResource;
+use App\Models\Greeting;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GreetingController extends Controller
 {
@@ -15,18 +20,27 @@ class GreetingController extends Controller
      */
     public function index()
     {
-        //
+        $greetings = Greeting::with(['sender','receiver'])->orderBy('created_at','desc')->paginate();
+        return GreetingResource::collection($greetings);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  StoreGreetingRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreGreetingRequest $request)
     {
-        //
+        $input = $request->validated();
+        $receiver = User::where('email', $input['receiver'])->first();
+        $greeting = new Greeting();
+        $greeting->title = $input['title'];
+        $greeting->text = $input['text'];
+        $greeting->sender_id = Auth::user()->id;
+        $greeting->receiver_id = $receiver->id;
+        $greeting->save();
+        return new GreetingResource($greeting);
     }
 
     /**
